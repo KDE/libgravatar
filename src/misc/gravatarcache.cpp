3acf28d0bdd2548bf3abf9d42b74bde677f0fc88
Montel Laurent <montel@kde.org>
2016-12-18 08:57:49 +0100

    Port to for (... : ...)

--- src/misc/gravatarcache.cpp
+++ src/misc/gravatarcache.cpp
@@ -128,7 +128,7 @@ void GravatarCache::clearAllCache()
         QDir dir(path);
         if (dir.exists()) {
             const QFileInfoList list = dir.entryInfoList();  // get list of matching files and delete all
-            Q_FOREACH (const QFileInfo &it, list) {
+            for (const QFileInfo &it : list) {
                 dir.remove(it.fileName());
             }
         }
