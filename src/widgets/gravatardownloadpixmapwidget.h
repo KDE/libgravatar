dc2bf7d65d149dd7c7c65b822c0e3fc16530d8d0
Laurent Montel <montel@kde.org>
2019-03-12 08:54:55 +0100

    Fix forward declaration

--- src/widgets/gravatardownloadpixmapwidget.h
+++ src/widgets/gravatardownloadpixmapwidget.h
@@ -25,7 +25,6 @@
 class QLabel;
 class QLineEdit;
 class QPushButton;
-class QCheckBox;
 namespace Gravatar {
 class GravatarResolvUrlJob;
 class GRAVATAR_EXPORT GravatarDownloadPixmapWidget : public QWidget
