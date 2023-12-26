5491af5eb7150a41b889657694f7796be785ce5b
Laurent Montel <montel@kde.org>
2023-12-26 07:43:39 +0100

    Fix the translation template version number (add 6 for kf6)
    Make sure that it will not conflict with qt5 version.
    
    @teams/localization

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 # SPDX-FileCopyrightText: none
 # SPDX-License-Identifier: BSD-3-Clause
-$XGETTEXT `find . -name '*.h' -o -name '*.cpp' ` -o $podir/libgravatar.pot
+$XGETTEXT `find . -name '*.h' -o -name '*.cpp' ` -o $podir/libgravatar6.pot
 rm -f rc.cpp
